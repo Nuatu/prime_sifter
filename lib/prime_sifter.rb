9fc5c9566a688580219a42617699805e93dca552
Nuatu Tseggai & Jeremy Whitaker <student@epicodus.com>
2014-07-24 14:30:51 -0700

    returns all primes up to the input provided by user.

--- lib/prime_sifter.rb
+++ lib/prime_sifter.rb
@@ -1,5 +1,13 @@
 def prime_sifter(input)
+  numberList1 =(2..input).to_a
+  numberList2 =(2..input).to_a
 
-  []
-
+  numberList1.each do |x|
+    numberList2.each do |y|
+      numberList1.delete(x*y)
+    end
+  end
+  numberList1
 end
+
+prime_sifter(100)
--- spec/prime_sifter_spec.rb
+++ spec/prime_sifter_spec.rb
@@ -8,8 +8,11 @@ describe("prime_sifter") do
     prime_sifter(1).class.should(eq(Array))
   end
 
-  # it("takes an input and returns all the prime numbers from 1 to the input in an array") do
-  #   prime_sifter(5).should(eq([1,2,3,5]))
-  # end
+  it("takes an input and returns all the prime numbers from 1 to the input in an array") do
+    prime_sifter(5).should(eq([2,3,5]))
+  end
 
+  it ("takes any value for n and returns all primes ") do
+    prime_sifter(11).should(eq([2, 3, 5, 7, 11]))
+  end
 end
